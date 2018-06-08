2799ce48d3226a61b41b740b8d5af3633ffc767f
Niamh Cahill <niamh.cahill@gmail.com>
2018-06-08 09:36:14 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # install_tomcat
 
 TODO: Enter the cookbook description here.
+1. Prerequisites/assumptions for Tomcat Installation Cookbook:
+- Target node is Centos 7
+- Target node has access to the interwebs
+- Chef server is Hosted Chef instance
+- root access on target node - understood that sudo would be more likely in production
+- chef-client executed locally on node
 
+2. Workshop summary & overview
+- Minimum viable product - cookbook works and can be executed multiple times without error
+- For production version, the following approach would be added:
+  - additional template files
+  - incorporate other recipes for Tomcat deployment (for example)
+  - less use of bash and execute - recursive changes to directory permissions would still be achieved through execute as guidance is that the Chef approach to recursive changes slows updates down in a production environment
+
+3. Recipe takes the following approach:
+- installs Java 1.7 JDK package using yum_package resource
+- creates group called tomcat to specify tomcat application specific permissions
+- creates a user called tomcat with the appropriate settings, shell etc.
+- pulls down the Apache Tomcat 8.5.20 installation tarball only if hasn't been updated since the last installation (unlikely but wanted to add some checks in there)
+- extracts the tarball
+- recursively changes directory and group permissions on tomcat files and directory
+- lays down the tomcat service file using tomcat.service.erb template
+- reloads the tomcat service configuration
+- enables the tomcat service and startit
+- verify that the tomcat server is responding on the appropriate localhost port -
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,87 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+#Install OpenJDK 7 JDK
+yum_package 'java-1.7.0-openjdk-devel' do
+        action :install
+end
+
+group 'tomcat' do
+  group_name 'tomcat'
+  gid '12345'
+end
+
+
+user 'tomcat' do
+  comment 'Tomcat User'
+  username 'tomcat'
+  gid 'tomcat'
+  shell '/bin/nologin'
+  home '/opt/tomcat'
+end
+
+remote_file '/tmp/apache-tomcat-8.5.20.tar.gz' do
+  source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz'
+  action :nothing
+end
+
+http_request 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz' do
+  message ''
+  url 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz'
+  action :head
+  if ::File.exist?('/tmp/apache-tomcat-8.5.20.tar.gz')
+    headers 'If-Modified-Since' => File.mtime('/tmp/apache-tomcat-8.5.20.tar.gz').httpdate
+  end
+  notifies :create, 'remote_file[/tmp/apache-tomcat-8.5.20.tar.gz]', :immediately
+end
+
+bash 'extract_module' do
+  cwd '/tmp'
+  code <<-EOH
+    mkdir -p /opt/tomcat
+    tar xzf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
+    EOH
+end
+
+execute "chgrp-tomcat" do
+  command "chgrp -R tomcat /opt/tomcat"
+  user "root"
+  action :run
+end
+
+execute "chmod-conf" do
+  command "chmod -R g+r /opt/tomcat/conf"
+  user "root"
+  action :run
+end
+
+execute "chmod-conf-gx" do
+  command "chmod g+x /opt/tomcat/conf"
+  user "root"
+  action :run
+end
+
+execute "chown-tomcat" do
+  cwd '/opt/tomcat'
+  command "chown -R tomcat webapps/ work/ temp/ logs/"
+  user "root"
+  action :run
+end
+
+template '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service.erb'
+  mode '0755'
+end
+
+systemd_unit 'tomcat.service' do
+  action :restart
+end
+
+service 'tomcat' do
+  action [:enable, :start]
+end
+
+http_request 'check_tomcat' do
+  url 'http://localhost:8080'
+end
